de76aefa55851dc0c251a677c3d62afd428a48e7
John Yeo <johnyeocx@gmail.com>
2024-05-28 15:19:02 +0100

    changes

--- server/test.py
+++ server/test.py
@@ -1,3 +1,4 @@
+import sys
 from optimisation.algorithm import load_policy_network_checkpoint, predict
 from optimisation.gen_data import getDayData, getTickData
 from optimisation.models import Day, Tick
@@ -18,26 +19,28 @@ env = {
 history = []
 overall_cost = 0
 costs_per_day = []
-while True:
-    tick = getTickData(day_id, tick_id)
-    cost, actions = predict(policy_network, env, tick, history)
 
-    overall_cost += cost
+print(sys.path)
+# while True:
+#     tick = getTickData(day_id, tick_id)
+#     cost, actions = predict(policy_network, env, tick, history)
 
-    print("Cost:", cost)
-    print("Overall cost:", overall_cost)
-    print("Imp / Exp:", actions["import_export"])
-    print("Rel / Sto:", actions["release_store"])
-    print("Allocations:", [round(a, 3) for a in actions["allocations"]])
-    print("-" * 20)
-    print()
-    time.sleep(2)
+#     overall_cost += cost
 
-    if tick_id == 60:
-        tick_id = 0
-        day_id += 1
-        day = getDayData(day_id)
-        env["deferables"] = day.deferables
-        costs_per_day.append(overall_cost)
-    else:
-        tick_id += 1
+#     print("Cost:", cost)
+#     print("Overall cost:", overall_cost)
+#     print("Imp / Exp:", actions["import_export"])
+#     print("Rel / Sto:", actions["release_store"])
+#     print("Allocations:", [round(a, 3) for a in actions["allocations"]])
+#     print("-" * 20)
+#     print()
+#     time.sleep(2)
+
+#     if tick_id == 60:
+#         tick_id = 0
+#         day_id += 1
+#         day = getDayData(day_id)
+#         env["deferables"] = day.deferables
+#         costs_per_day.append(overall_cost)
+#     else:
+#         tick_id += 1
