91c8012732628ada5718cee93ab8628c55bf1d63
Jay <jay_mistry@live.co.uk>
2024-05-30 17:56:48 +0100

    latest

--- run.sh
+++ run.sh
@@ -2,11 +2,5 @@
 # ngrok http 8080 --scheme http &
 # wait
 
-<<<<<<< HEAD
-# python server/test.py
-python3 server/main.py
-# python3 server/parallel_get.py
-=======
 python server/test.py
 # python server/main.py
->>>>>>> d68b9f8 (made changes to rl algorithm like abs allocation values)
