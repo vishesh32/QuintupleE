9f24f9e63c4d17ecf3347691123d7eeb49248615
Intishar <49362508+intisharalam@users.noreply.github.com>
2024-06-11 23:00:01 +0100

    fix

--- Hardware/mqtt_client.py
+++ Hardware/mqtt_client.py
@@ -3,7 +3,10 @@ import ubinascii
 from wifi import init
 import json
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 
 >>>>>>> Stashed changes
 # uncomment this line to import the helper functions
